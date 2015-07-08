31742dacce3d8506f57d3112fa7fb0567ba6ca8f
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-07 23:08:43 -0500

    Sumeet's html tabs

---
+++ sumeets.js
@@ -0,0 +1,20 @@
+// $(function(){
+//   $(".tabs a").click(function(e){
+//     e.preventDefault();
+//     alert("This worked!");
+//   });
+//
+//   $("#about").click(function(e){
+//     e.preventDefault();
+//     alert("This worked!");
+//   });
+// });
+//
+//
+// $jQuery('a').click(function(e){
+//   alert("This worked!");
+//   e.preventDefault();
+//
+// });
+
+$('a').click(alert("Yes!"));
